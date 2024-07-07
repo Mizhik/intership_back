aec3d1797f8f4f90f1295e401ce3d50a132e67ad
Mizhik <vlad.mezhinskiy07@gmail.com>
2024-07-07 17:05:03 +0300

    add postgres and redis, add docker-compose

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from app.core.settings import config
+from redis import asyncio as aioredis
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.settings import config
+from app.database.db import get_db, get_redis_client
 app = FastAPI()
 
 origins = [config.ALLOWED_ORIGINS]
@@ -16,9 +21,31 @@ app.add_middleware(
 )
 
 
-@app.get("/api/healthchecker")
+@app.get("/")
 def root():
     return {"status_code": 200, "detail": "ok", "result": "working"}
+
+
+@app.get("/api/healthchecker/db")
+async def healthchecker(db: AsyncSession = Depends(get_db)):
+    try:
+        result = await db.execute(text("SELECT 1"))
+        result = result.fetchone()
+        if result is None:
+            raise HTTPException(
+                status_code=500, detail="Database is not configured correctly"
+            )
+        return {"message": "Welcome to FastAPI!"}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Error connecting to the database")
+
+
+@app.get("/api/healthchecker/redis")
+async def read_root(redis: aioredis.Redis = Depends(get_redis_client)):
+    await redis.set("mykey", "value")
+    value = await redis.get("mykey")
+    return {"message": f"Value from Redis: {value}"}
 
 
 if __name__ == "__main__":
